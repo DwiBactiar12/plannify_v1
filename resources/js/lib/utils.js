4d739d83e174298b669215b6f74ee2a4f8b14dbd
m dwi bactiar fajar <dwi.bactiar@ilcs.co.id>
2025-02-01 00:14:04 +0700

    configuration setup

--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+
+export const STATUS={
+    TODO:"To Do",
+    IN_PROGRESS:"In Progress",
+    DONE:"Done",
+    ONREVIEW:"On Review",
+    UNKNOWN:"Unknown"
+}
+
+
+export const PRIORITY={
+    LOW:"Low",
+    MEDIUM:"Medium",
+    HIGH:"High",
+    UNKNOWN:"Unknown"
+}
