4c9e3c3d8c26f44905a4cfc0bd8c08f4c4972841
m dwi bactiar fajar <dwi.bactiar@ilcs.co.id>
2025-02-09 02:58:58 +0700

    sidebar

--- resources/js/Layouts/AppLayout.jsx
+++ resources/js/Layouts/AppLayout.jsx
@@ -2,10 +2,33 @@ import { Avatar, AvatarFallback } from '@/Components/ui/avatar';
 import { Dialog, Transition } from '@headlessui/react';
 import { Head, Link } from '@inertiajs/react';
 import { Fragment, useState } from 'react';
-import { PiSidebar, PiX } from 'react-icons/pi';
+import { PiLockOpen, PiPlus, PiSidebar, PiSquaresFour, PiUser, PiX } from 'react-icons/pi';
 
 export default function AppLayout({ children, title }) {
     const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    const menus = [
+        {
+            title: 'Dashboard',
+            href: '/',
+            icon: PiSidebar,
+        },
+        {
+            title: 'People',
+            href: '/',
+            icon: PiUser,
+        },
+        {
+            title: 'My Task',
+            href: '/',
+            icon: PiSquaresFour,
+        },
+        {
+            title: 'Logout',
+            href: '/',
+            icon: PiLockOpen,
+        },
+    ];
 
     return (
         <>
@@ -74,6 +97,65 @@ export default function AppLayout({ children, title }) {
                             </Link>
                         </div>
                         {/* Sidebar */}
+
+                        <nav className="flex flex-col flex-1">
+                            <ul role="list" className="flex flex-col flex-1 gap-y-7">
+                                <li>
+                                    <ul role="list" className="-mx-2 space-y-2">
+                                        {/* menu */}
+
+                                        {menus.map((menu) => (
+                                            <li>
+                                                <Link
+                                                    href={menu.href}
+                                                    className="flex  p-3 text-sm font-semibold leading-relaxed tracking-tighter text-foreground hover:bg-gray-100 rounded-md group gap-x-3 "
+                                                >
+                                                    <menu.icon className="w-6 h-6 text-foreground shrink-0" />
+                                                    {menu.title}
+                                                </Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </li>
+                                <li>
+                                    {/* workspaces */}
+                                    <div className="flex justify-between items-center">
+                                        <div className="text-xs font-smeibold leading-relaxed uppercase text-foreground">
+                                            Workspaces
+                                        </div>
+
+                                        <Link>
+                                            <PiPlus className="w-4 h-4 text-foreground hover:text-red-500" />
+                                        </Link>
+                                    </div>
+                                    <ul role="list" className="mt-2 -mx-2 space-y-2">
+                                        <li>
+                                            <Link
+                                                href="#"
+                                                className="flex w-full p-3 text-sm font-semibold leading-relaxed rounded-md text-foreground hover:bg-gray-100 gap-x-3"
+                                            >
+                                                <span className="border-foreground text-foreground flex h-6 w-6  shrink-0 items-center justify-center rounded-lg  border bg-white text-[0.625rem] font-medium">
+                                                    B
+                                                </span>
+                                                <span className="truncate">Backed Developer</span>
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li className="mt-auto -mx-6">
+                                    {/* profile */}
+                                    <Link
+                                        href="#"
+                                        className="flex items-center px-6 py-3 text-sm font-semibold leading-relaxed  text-foreground hover:bg-gray-100 gap-x-4"
+                                    >
+                                        <Avatar>
+                                            <AvatarFallback>X</AvatarFallback>
+                                        </Avatar>
+                                        <span> John Doe</span>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </nav>
                     </div>
                 </div>
 
