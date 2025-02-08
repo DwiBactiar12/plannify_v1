6246b40d4f9e6f2f6497df09c744d89a89c17889
m dwi bactiar fajar <dwi.bactiar@ilcs.co.id>
2025-02-08 19:38:34 +0700

    components base

--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -2,22 +2,23 @@ import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
 
 
-export const STATUS={
-    TODO:"To Do",
-    IN_PROGRESS:"In Progress",
-    DONE:"Done",
-    ONREVIEW:"On Review",
-    UNKNOWN:"Unknown"
+export const STATUS = {
+    TODO: "To Do",
+    INPROGRESS: "In Progress",
+    DONE: "Done",
+    ONREVIEW: "On Review",
+    UNKNOWN: "Unknown"
 }
 
 
-export const PRIORITY={
-    LOW:"Low",
-    MEDIUM:"Medium",
-    HIGH:"High",
-    UNKNOWN:"Unknown"
+export const PRIORITY = {
+    URGENT: 'Urgent',
+    LOW: "Low",
+    MEDIUM: "Medium",
+    HIGH: "High",
+    UNKNOWN: "Unknown"
 }
